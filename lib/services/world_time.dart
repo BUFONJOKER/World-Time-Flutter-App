429093b805672102a5b0e3b5d9048ea6ffabc8c6
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-11-06 21:11:38 +0500

    time format corrected

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,6 +1,7 @@
 import 'package:http/http.dart';
 import 'dart:convert';
 import 'dart:developer';
+import 'package:intl/intl.dart';
 
 class WorldTime{
   String? location; // location name for the UI
@@ -16,26 +17,30 @@ class WorldTime{
       // log(data.toString());
       // get properties from data
       String datetime = data['datetime'];
-      // log(datetime);
-      // log(offset);
-      // create DateTime object
-      // log('now $now');
+      String offset = data['utc_offset'];
+      // Parse the string into a DateTime object
+      DateTime dateTime = DateTime.parse(datetime);
+
+      // Parse the offset string and extract hours and minutes
+      int hours = int.parse(offset.substring(1, 3)); // Extract hours
+      int minutes = int.parse(offset.substring(4, 6)); // Extract minutes
+
+      // Create a Duration using the parsed hours and minutes
+      Duration timeOffset = Duration(hours: hours, minutes: minutes);
+
+      // Adjust the DateTime by adding the offset
+      DateTime adjustedDateTime = dateTime.add(timeOffset);
+
+      // Format the adjusted DateTime object into a 12-hour time format
+      String formattedTime = DateFormat('hh:mm a').format(adjustedDateTime);
+
+       // Output the formatted time with the offset
       int hour = int.parse(datetime.substring(11,13));
+
       isDayTime = hour > 6 && hour < 20  ? true : false;
-      // log(now.toString());
-      // time = DateFormat.jm().format(now);
-      int checkTime = datetime.substring(11,16).compareTo("12:00");
-      // log('checkTime $checkTime');
-      if(checkTime != 1){
-        int hour = int.parse(datetime.substring(11,13));
-        time = '$hour${datetime.substring(13,16)} AM';
-      }
-      else{
-        int change24hourTo12 = int.parse(datetime.substring(11,13));
-        change24hourTo12 = change24hourTo12 - 12;
-        // String string = number.toString();
-        time = '$change24hourTo12${datetime.substring(13,16)} PM';
-      }
+
+      time = formattedTime;
+
     } on Exception catch (e) {
       log('caught error: $e');
     }
